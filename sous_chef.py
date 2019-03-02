1569bfc547ebb5ad4f30d794e9098513b757a88d
ryan1117001 <ryan1117001@gmail.com>
2019-03-02 15:16:47 -0500

    sous_chef.py calibration() fix

--- sous_chef.py
+++ sous_chef.py
@@ -13,7 +13,11 @@ from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
 from time import sleep
 from hx711 import HX711
 
-
+def toZero(number):
+    if (number < 0):
+        return 0
+    else:
+        return number
 def cleanAndExit():
     print "Cleaning..."
     GPIO.cleanup()
@@ -23,25 +27,31 @@ def cleanAndExit():
 
 def handleCalibration():
     stepperOne.ChangeDutyCycle(50)
-    time.sleep(1)
+    time.sleep(ONE_PADDLE_DIVET)
     stepperOne.ChangeDutyCycle(0)
+
+    # let crumbs and last bit to fall
+    time.sleep(1)
 
     # get value in grams
     val = hx.get_weight(5)
     print(val, "grams after one revolution")
     # rate at which the ingredient falls.
     # one revolution is one second
-    FOOD_RATE_ONE = val
+    FOOD_RATE_ONE = toZero(val)
     # hx reset
     hx.power_down()
     hx.power_up()
 
 def handleDispense(inputted_grams):
+    
     drop_time = inputted_grams / FOOD_RATE_ONE
-        
-    # May need to figure out a way to close it
-    close_time = drop_time - ((drop_time % ONE_PADDLE_DIVET) * ONE_PADDLE_DIVET)
 
+    # May need to figure out a way to close it
+    close_time = drop_time % ONE_PADDLE_DIVET
+    
+    print(close_time)
+    
     # move motor
     stepperOne.ChangeDutyCycle(50)
     time.sleep(drop_time)
@@ -54,8 +64,13 @@ def handleDispense(inputted_grams):
     stepperOne.ChangeDutyCycle(0)
     GPIO.output(DIR, GPIO.LOW)
 
+    # let crumbs/ingredient to fall
+    time.sleep(1)
+
 class MotorEcho(WebSocket):
     def handleMessage(self):
+        # reset the load cell
+        hx.tare()
 
         # if statement to see which button was pressed
         if (self.data == "calibration"):
@@ -66,27 +81,37 @@ class MotorEcho(WebSocket):
             
             # Amount of time needed to sleep
             desired_grams = ingredients[0]['grams']
-            
-            # get value in grams
-            val = hx.get_weight(5)
-            print(val, "grams")
+ 
+            # current amount on the scale
+            # should have been tared to 0
+            val = 0
+
+            # previous value on the scale
+            prev_val = 0
 
             # dispenses within a margiin of 10 grams
             # may want to look into this
-            while ((desired_grams - val) > 10):
-                handleDispense(desired_grams - val)
 
+            while (val < desired_grams - 5):
+                handleDispense(desired_grams - val)
+    
+                # current val to prev_val
+                prev_val = val
+                print(prev_val, val)
                 # get value in grams
-                val = hx.get_weight(5)
+                val = toZero(hx.get_weight(5))
                 print(val, "grams")
 
+                if (prev_val == val):
+                    self.sendMessage("alert")
+                    return
 
             # hx reset
             hx.power_down()
             hx.power_up()
         
         # send packet
-        self.sendMessage("Completed")
+        self.sendMessage("completed")
 
     def handleConnected(self):
         print(self.address, 'connected')
@@ -124,13 +149,12 @@ GPIO.output(DIR, GPIO.LOW)
 stepperOne = GPIO.PWM(STEP, 200)
 stepperOne.start(0)
 
-# Ingredient grams/revolution
-# This equates to the amount of food that comes out at one revolution
-# At 200 Hz, that is one revolution per second.
+# Ingredient grams / ONE_PADDLE_DIVET
+# This equates to the amount of food that comes out at one paddle divet
 FOOD_RATE_ONE = 1
 
 # Time constants for the motor
-ONE_PADDLE_DIVET = 1/6
+ONE_PADDLE_DIVET = float(1)/6
 
 # server setup
 server = SimpleWebSocketServer('10.1.250.128', 8000, MotorEcho)
